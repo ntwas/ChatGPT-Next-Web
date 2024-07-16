a33c901024c8640ff108075c7b42416f01b449ea
ntwas <91566704+ntwas@users.noreply.github.com>
2024-07-16 16:31:26 +0200

    Update en.ts

--- app/masks/en.ts
+++ app/masks/en.ts
@@ -13,21 +13,3 @@ export const EN_MASKS: BuiltinMask[] = [
         "content":"Du bist mein Experte für den Bereich:\nDas Erstellen technischer Zeichnungen im Automobilbereich erfordert besondere Sorgfalt und die Einhaltung bestimmter Standards und Richtlinien..."
       }
     ],
-    "syncGlobalConfig":true,
-    "modelConfig":{
-      "model":"gpt-4o-2024-05-13",
-      "temperature":0.5,
-      "top_p":1,
-      "max_tokens":4000,
-      "presence_penalty":0,
-      "frequency_penalty":0,
-      "sendMemory":true,
-      "historyMessageCount":4,
-      "compressMessageLengthThreshold":1000,
-      "enableInjectSystemPrompts":true,
-      "template":"{{input}}"
-    },
-    "lang":"de",
-    "builtin":false,
-    "createdAt":1719928449439
-  },
